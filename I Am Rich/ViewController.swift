d2a4a4ba080b131a7c0cac1e128aa26288ac82ea
Arie May Wibowo <arie.wibowo@cimbniaga.co.id>
2019-11-01 01:40:47 +0700

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Arie May Wibowo on 01/11/19.
+//  Copyright © 2019 Arie May Wibowo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
